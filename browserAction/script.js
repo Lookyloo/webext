70e6b99d4f81a2e1094cbebcc00795116507348f
Raphaël Vinot <raphael@vinot.info>
2021-11-17 15:45:04 -0800

    chg: Cleanup, run linter

--- browserAction/script.js
+++ browserAction/script.js
@@ -9,56 +9,58 @@ async function onGot(tabs){
   const referrer = await browser.tabs.executeScript(tab.id, {
         code: 'document.referrer'
   }).then(result => result[0]);
-
   console.log(`Referer: ${referrer}`);
 
-  var gettingAllCookies =  browser.cookies.getAll({storeId: tab.cookieStoreId});
-  gettingAllCookies.then((cookies) => {
-    console.log(cookies);
-    console.log(document.referrer);
-    var lookylooURL = browser.storage.sync.get();
-    lookylooURL.then((res) => {
-      console.log(res);
-      console.log(`${res.url}/submit`);
-      let data = {url: tab.url , listing: 0}
-      if (pass_referrer === true) {
-          data.referer = referrer;
-      }
-      if (pass_cookies === true) {
-          data.cookies = cookies
-      }
-      if (pass_ua === true) {
-          data.user_agent = navigator.userAgent;
-      }
-      console.log(data);
+  const cookies = await browser.cookies.getAll({
+      storeId: tab.cookieStoreId
+  }).then((cookies) => cookies);
+  console.log(cookies);
 
-      fetch(`${res.url}/submit`, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        // mode: 'cors', // no-cors, *cors, same-origin
-        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'include', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          // 'Accept': 'text/text',
-          // 'Access-Control-Allow-Origin': 'http://127.0.0.1:5100'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      })
-        .then(response => response.json())
-        .then(result => {
-            setTimeout(function() {
-                var creating = browser.tabs.create({
-                    url :  `${res.url}/tree/${result}`
-                });
-                creating.then(tab => {console.log(`Created new tab: ${tab.id}`)},
-                              error => {console.log(`Error: ${error}`)})
-            }, 200);
-        });
+  const config = await browser.storage.sync.get().then(res => res);
+  console.log(config);
+  console.log(`${config.url}/submit`);
+
+  let data = {url: tab.url , listing: 0}
+  if (pass_referrer === true) {
+      data.referer = referrer;
+  }
+  if (config.private && pass_cookies === true) {
+      data.cookies = cookies
+  }
+  if (pass_ua === true) {
+      data.user_agent = navigator.userAgent;
+  }
+  console.log(data);
+
+  await fetch(`${config.url}/submit`, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    // mode: 'cors', // no-cors, *cors, same-origin
+    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: 'include', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Accept': 'text/text',
+      // 'Access-Control-Allow-Origin': 'http://127.0.0.1:5100'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    // redirect: 'follow', // manual, *follow, error
+    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+    .then(response => response.json())
+    .then(result => {
+        setTimeout(function() {
+            var creating = browser.tabs.create({
+                url: `${config.url}/tree/${result}`
+            });
+            creating.then(tab => {console.log(`Created new tab: ${tab.id}`)},
+                          error => {console.log(`Error: ${error}`)})
+        }, 200);
+    })
+    .catch((error) => {
+        document.querySelector(".error-submit").textContent = `Unable to reach the instance: ${error}`;
+        document.querySelector("#capture").disabled = false;
     });
-  });
 };
 
 function onError(error) {
@@ -75,10 +77,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   var lookylooURL = browser.storage.sync.get();
   lookylooURL.then((res) => {
-      document.querySelector(".stored-url").innerHTML = `Current instance: ${res.url}`
+      document.querySelector(".stored-url").textContent = `Current instance: ${res.url}`
   });
-  document.querySelector(".current-ua").innerHTML = `Current User-Agent: ${navigator.userAgent}`
-  // document.querySelector(".current-referer").innerHTML = `Current referer: ${}`
+  document.querySelector(".current-ua").textContent = `Current User-Agent: ${navigator.userAgent}`
+  browser.storage.sync.get().then(res => {
+      if (!res.private) {
+        document.querySelector(".cookie-box").hidden = true;
+      }
+  });
 
   document.querySelector("#capture").addEventListener('click', launchCapture);
 });
--- options/script.js
+++ options/script.js
@@ -1,20 +1,19 @@
 function saveOptions(e) {
   browser.storage.sync.set({
-    url: document.querySelector("#url").value
+    url: document.querySelector("#url").value,
+    private: document.querySelector("#private").checked
   });
   e.preventDefault();
   document.querySelector("#stored-url").innerText = document.querySelector("#url").value;
 }
 
 function restoreOptions() {
-  var storageItem = browser.storage.sync.get('url');
+  var storageItem = browser.storage.sync.get();
   storageItem.then((res) => {
     document.querySelector("#stored-url").innerText = res.url;
-  });
-
-  var gettingItem = browser.storage.sync.get('url');
-  gettingItem.then((res) => {
     document.querySelector("#url").value = res.url || 'http://127.0.0.1:5100';
+
+    document.querySelector("#private").checked = res.private;
   });
 }
 
